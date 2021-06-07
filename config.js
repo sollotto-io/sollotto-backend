c4afc703c36c69dc6181c55ae805c1751e80085c
RushiChandalia <rushichandalia@gmail.com>
2021-06-07 23:06:14 +0530

    hi

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     MONGODB:
-      `mongodb://LeadDev:${process.env.MONGO_PASS}@cluster0-shard-00-00.g0kih.mongodb.net:27017,cluster0-shard-00-01.g0kih.mongodb.net:27017,cluster0-shard-00-02.g0kih.mongodb.net:27017/Sollotto-offchain?ssl=true&replicaSet=atlas-8ehri1-shard-0&authSource=admin&retryWrites=true&w=majority`,
+      `mongodb://LeadDev:y6HEdCJgDH9RdQB0@cluster0-shard-00-00.g0kih.mongodb.net:27017,cluster0-shard-00-01.g0kih.mongodb.net:27017,cluster0-shard-00-02.g0kih.mongodb.net:27017/Sollotto-offchain?ssl=true&replicaSet=atlas-8ehri1-shard-0&authSource=admin&retryWrites=true&w=majority`,
     CHARITY_STATUS :{
           IN_QUE: "IN QUE",
           VOTE_NOW:"VOTE NOW",
