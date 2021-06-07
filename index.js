30b735290c10038e69c412d334591a45a17a8300
RushiChandalia <rushichandalia@gmail.com>
2021-06-07 23:40:03 +0530

    cors changes

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const cors = require('cors')
 
 
 const app = express()
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 app.use(cors()) // not having cors enabled will cause an access control error
 const server = new ApolloServer({
 	typeDefs,
