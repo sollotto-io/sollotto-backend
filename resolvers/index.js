606980ba7716be09edc92469d7a9803bffe73362
Kashyap Pavra <pavrakashyap@gmail.com>
2021-06-15 22:12:35 +0530

    charity.js files capitalised

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,23 +1,18 @@
-
-const charityResolvers = require("./charity")
-const ticketResolvers = require('./ticket')
-const lotteryResolvers = require('./lottery')
-const drawingResolvers = require('./drawing')
+const charityResolvers = require("./Charity");
+const ticketResolvers = require("./ticket");
+const lotteryResolvers = require("./lottery");
+const drawingResolvers = require("./drawing");
 module.exports = {
-   
-    Mutation: {
-        
-        ...charityResolvers.Mutation,
-        ...ticketResolvers.Mutation,
-      ...drawingResolvers.Mutation,
-        ...lotteryResolvers.Mutations
-
-    },
-    Query:{
-        ...charityResolvers.Query,
-        ...drawingResolvers.Query,
-        // ...ticketResolvers.Query,
-        ...lotteryResolvers.Query
-    }
-
-}
+	Mutation: {
+		...charityResolvers.Mutation,
+		...ticketResolvers.Mutation,
+		...drawingResolvers.Mutation,
+		...lotteryResolvers.Mutations,
+	},
+	Query: {
+		...charityResolvers.Query,
+		...drawingResolvers.Query,
+		// ...ticketResolvers.Query,
+		...lotteryResolvers.Query,
+	},
+};
