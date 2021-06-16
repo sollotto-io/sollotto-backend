ef6f01601836ee7608c1c4414f8f8fe33379c12c
RushiChandalia <rushichandalia@gmail.com>
2021-06-16 14:41:58 +0530

    new cluster added and final db deployment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     MONGODB:
-      `mongodb://RushiC:rushi.c004@cluster0-shard-00-00.1fjri.mongodb.net:27017,cluster0-shard-00-01.1fjri.mongodb.net:27017,cluster0-shard-00-02.1fjri.mongodb.net:27017/sollotto-offchain?ssl=true&replicaSet=atlas-12x6gn-shard-0&authSource=admin&retryWrites=true&w=majority`,
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster1-shard-00-00.g0kih.mongodb.net:27017,cluster1-shard-00-01.g0kih.mongodb.net:27017,cluster1-shard-00-02.g0kih.mongodb.net:27017/sollotto-offchain?ssl=true&replicaSet=atlas-k0g4lq-shard-0&authSource=admin&retryWrites=true&w=majority`,
     CHARITY_STATUS :{
           IN_QUE: "IN QUE",
           VOTE_NOW:"VOTE NOW",
