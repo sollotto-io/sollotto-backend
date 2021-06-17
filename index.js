5c6768e8474a81e17afbacbcfa362edd09e91089
RushiChandalia <rushichandalia@gmail.com>
2021-06-17 20:29:55 +0530

    cron changed

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ mongoose
       })
       .then(() => {
         console.log("inside cron then");
-        cron.schedule("*/1 * * * *", () => {changeDraw()},
-        // {
-        //   scheduled: true,
-        //   timezone: "Atlantic/Azores"
-        // }
+        cron.schedule("0 0 * * wed,sat", () => {changeDraw()},
+        {
+          scheduled: true,
+          timezone: "Atlantic/Azores"
+        }
         );
       })
       .catch((err) => {
