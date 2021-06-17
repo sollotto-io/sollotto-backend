c99c0432e66c2c0019f6d98999abe0a2c8a29723
rushi.chandalia <rushi.chandalia@DSK-711>
2021-06-17 10:45:50 +0530

    mongodb connection string transferred to env

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     MONGODB:
-      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster1-shard-00-00.g0kih.mongodb.net:27017,cluster1-shard-00-01.g0kih.mongodb.net:27017,cluster1-shard-00-02.g0kih.mongodb.net:27017/sollotto-offchain?ssl=true&replicaSet=atlas-k0g4lq-shard-0&authSource=admin&retryWrites=true&w=majority`,
+      process.env.DEVELOPMENT,
     CHARITY_STATUS :{
           IN_QUE: "IN QUE",
           VOTE_NOW:"VOTE NOW",
