aa0d482938db23729354b1bd6c298db0752790e8
rushi.chandalia <rushi.chandalia@DSK-711>
2021-06-21 17:15:45 +0530

    lottery id changed

--- utils/changeDraw.js
+++ utils/changeDraw.js
@@ -35,7 +35,7 @@ const closeDrawing = async (drawing) => {
   });
   if (result.winFlag === false) {
     await Lottery.findByIdAndUpdate(
-      "60c447da624b8a3d5095baa8",
+      "60c9be158676ea0799255ee4",
       { $inc: { TotalPoolValue: drawing.TotalPoolValue * 0.65 } },
       { new: true }
     );
@@ -53,7 +53,7 @@ const closeDrawing = async (drawing) => {
     );
   } else {
     await Lottery.findByIdAndUpdate(
-      "60c447da624b8a3d5095baa8",
+      "60c9be158676ea0799255ee4",
       { TotalPoolValue: 0 },
       { new: true }
     );
@@ -87,7 +87,7 @@ exports.changeDraw = async () => {
 };
 
 const openDrawing = async (activeDrawing) => {
-  const lottery = await Lottery.findById("60c447da624b8a3d5095baa8");
+  const lottery = await Lottery.findById("60c9be158676ea0799255ee4");
   var day = moment(activeDrawing.EndDate).format("dddd");
   
   const charityVote = [];
