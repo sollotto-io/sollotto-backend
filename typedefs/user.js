432d1f82ff05487d4285505882eb0be601268d43
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-07-15 22:02:52 -0600

    Finishing charity voting implementation

--- typedefs/user.js
+++ typedefs/user.js
@@ -3,11 +3,13 @@ const gql = require("graphql-tag");
 module.exports = gql`
   type User {
     id: ID!
-    UserPK:String
-    TokenValue:Int
+    UserPK: String
+    TokenValue: Int
   }
-extend type Query {
-    addUser(Userpk:String): Int!
-}
-
+  extend type Query {
+    addUser(Userpk: String): Int!
+  }
+  extend type Query {
+    getSingleUser(UserPK: String): User
+  }
 `;
