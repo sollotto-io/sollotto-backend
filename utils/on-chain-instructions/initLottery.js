e889cfabb5daf0ad391c71c573e80e8ab6a1b6bb
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-07-30 15:39:16 -0600

    adding Raffles

--- utils/on-chain-instructions/initLottery.js
+++ utils/on-chain-instructions/initLottery.js
@@ -9,50 +9,59 @@ const {
 } = require("@solana/web3.js");
 var CryptoJS = require("crypto-js");
 const { connection } = require("../../config.js");
-const BufferLayout  =  require('buffer-layout')
+const BufferLayout = require("buffer-layout");
 
 const initLottery = async (charities) => {
-  console.log(charities)
+  console.log(charities);
 
   // getting the signerAccount details
 
-  const HoldingWalletBytes = CryptoJS.AES.decrypt(process.env.HOLDING_WALLET_SECRETKEY, process.env.SECRET_KEY);
-  const HoldingWalletDecryptedText = JSON.parse(HoldingWalletBytes.toString(CryptoJS.enc.Utf8));
+  const HoldingWalletBytes = CryptoJS.AES.decrypt(
+    process.env.HOLDING_WALLET_SECRETKEY,
+    process.env.SECRET_KEY
+  );
+  const HoldingWalletDecryptedText = JSON.parse(
+    HoldingWalletBytes.toString(CryptoJS.enc.Utf8)
+  );
   const HoldingWallet = new Account(HoldingWalletDecryptedText);
-
 
   //getting public keys for data....
 
-  const rewards_wallet = new PublicKey(process.env.REWARD_WALLET_PUBLIC_KEY)
-  const slot_holders_rewards_wallet = new PublicKey(process.env.SLOT_HOLDER_REWARDS_PUBLIC_KEY)
-  const sollotto_labs_wallet = new PublicKey(process.env.SOLLOTTO_LABS_PUBLIC_KEY )
+  const rewards_wallet = new PublicKey(process.env.REWARD_WALLET_PUBLIC_KEY);
+  const slot_holders_rewards_wallet = new PublicKey(
+    process.env.SLOT_HOLDER_REWARDS_PUBLIC_KEY
+  );
+  const sollotto_labs_wallet = new PublicKey(
+    process.env.SOLLOTTO_LABS_PUBLIC_KEY
+  );
 
   //Solana Program id public key
-console.log(process.env.SOLANA_INIT_LOTTERY_PROGRAM)
+  console.log(process.env.SOLANA_INIT_LOTTERY_PROGRAM);
   const solanaProgramId = new PublicKey(
     process.env.SOLANA_INIT_LOTTERY_PROGRAM
   );
-console.log(solanaProgramId)
+  console.log(solanaProgramId);
 
   try {
-    console.log("working")
- // creating the object for Instruction data
+    console.log("working");
+    // creating the object for Instruction data
 
     const lotteryFields = {
-      lottery_id: parseInt(Math.floor(new Date().valueOf() * Math.random())/100000),
+      lottery_id: parseInt(
+        Math.floor(new Date().valueOf() * Math.random()) / 100000
+      ),
       charity_1: new PublicKey(charities[0]).toBytes(),
       charity_2: new PublicKey(charities[1]).toBytes(),
       charity_3: new PublicKey(charities[2]).toBytes(),
       charity_4: new PublicKey(charities[3]).toBytes(),
-      holding_wallet:HoldingWallet.publicKey,
-      rewards_wallet:rewards_wallet,
-      slot_holders_rewards_wallet:slot_holders_rewards_wallet,
-      sollotto_labs_wallet:sollotto_labs_wallet
-    
-    }
+      holding_wallet: HoldingWallet.publicKey,
+      rewards_wallet: rewards_wallet,
+      slot_holders_rewards_wallet: slot_holders_rewards_wallet,
+      sollotto_labs_wallet: sollotto_labs_wallet,
+    };
     //converting data into Buffer to be passed in instruction
 
-    dataArr = new Buffer.alloc(296,lotteryFields)
+    dataArr = new Buffer.alloc(296, lotteryFields);
 
     //create a new lotteryData account
 
@@ -70,7 +79,7 @@ console.log(solanaProgramId)
       newAccountPubkey: lotteryDataAccount.publicKey,
       programId: solanaProgramId,
     });
-   
+
     //creating the transaction instruction fro lottery data account
 
     const initLotteryTx = new TransactionInstruction({
@@ -86,14 +95,14 @@ console.log(solanaProgramId)
       data: dataArr,
     });
 
-    // creating transaction 
+    // creating transaction
 
     const transaction = new Transaction().add(
       createLotteryDataAccountTx,
       initLotteryTx
     );
-    //sending and confirming the transaction 
-    
+    //sending and confirming the transaction
+
     const confirmation = await sendAndConfirmTransaction(
       connection,
       transaction,
@@ -103,9 +112,15 @@ console.log(solanaProgramId)
         preflightCommitment: "singleGossip",
       }
     );
-    console.log(confirmation)
-  var lotteryDataAccountSKString = CryptoJS.AES.encrypt(JSON.stringify(Buffer.from(lotteryDataAccount.secretKey).toJSON().data), process.env.SECRET_KEY).toString();
-    return {lotteryDataSK : lotteryDataAccountSKString, lotteryId:lotteryFields.lottery_id };
+    console.log(confirmation);
+    var lotteryDataAccountSKString = CryptoJS.AES.encrypt(
+      JSON.stringify(Buffer.from(lotteryDataAccount.secretKey).toJSON().data),
+      process.env.SECRET_KEY
+    ).toString();
+    return {
+      lotteryDataSK: lotteryDataAccountSKString,
+      lotteryId: lotteryFields.lottery_id,
+    };
   } catch (e) {
     console.warn(e);
     console.log(`Error: ${e.message}`);
