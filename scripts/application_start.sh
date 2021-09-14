a867c59c8736e90abe2aaa730ee9ef3c99a1340f
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-09-14 12:21:06 -0600

    cd ci test again

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,8 +11,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+
+#install pm2
+ npm install pm2@latest -g
+
 #install node modules
 npm install
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+node index.js > app.out.log 2> app.err.log < /dev/null & 
+
+pm2 restart index.js
