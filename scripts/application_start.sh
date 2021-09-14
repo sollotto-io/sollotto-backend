bf8a82483ab0635d547c7d15c57447b46f377528
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-09-14 11:57:57 -0600

    test with pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,8 +11,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+#install pm2
+npm install pm2 -g
+
 #install node modules
 npm install
 
 #start our node app in the background
-node index.js > app.out.log 2> app.err.log < /dev/null & 
+pm2 start index.js > app.out.log 2> app.err.log < /dev/null & 
